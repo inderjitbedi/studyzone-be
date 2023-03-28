b4856c9a07ffebbe40ea8cd207514cc88fb9c432
Inderjit Bedi <iinderjitbedi@gmail.com>
2023-03-28 17:34:12 -0400

    angular build folder setting

--- app.js
+++ app.js
@@ -9,12 +9,10 @@ const authRoute = require('./routes/authRoute')
 const adminRoute = require('./routes/adminRoute')
 const swagger = require('./swagger');
 
-
 dotenv.config();
 
 const app = express();
 app.use(express.json());
-
 
 var originsWhitelist = [
     '*',
@@ -37,16 +35,17 @@ swagger(app);
 
 mongoose.connect(process.env.MONGODB_URI);
 mongoose.connection.on("connected", () => {
-    console.log("Connected to DB")
+    console.log("Connected to DB");
 });
 mongoose.connection.on("error", err => {
-    console.log("DB connection failed: ", err)
+    console.log("DB connection failed: ", err);
 });
 
 app.use('/api/auth', authRoute);
 app.use('/api/admin', adminRoute);
 app.use("/temp",express.static(path.join(__dirname, 'temp')));
 app.use("/slide",express.static(path.join(__dirname, 'uploads')));
+app.use("/",express.static(path.join(__dirname, 'public')));
 
 app.listen(3005, () => {
     console.log('Server started on port 3005');
